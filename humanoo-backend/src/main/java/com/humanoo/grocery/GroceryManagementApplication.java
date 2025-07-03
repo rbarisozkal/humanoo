deab8dafad6af46990576d4185a092c2d983cb57
rbarisozkal <barisozkal11@gmail.com>
2025-07-03 02:22:41 +0200

    feat(backend-init): grocery management backend with Maven setup,  dependencies

---
+++ humanoo-backend/src/main/java/com/humanoo/grocery/GroceryManagementApplication.java
@@ -0,0 +1,13 @@
+package com.humanoo.grocery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroceryManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroceryManagementApplication.class, args);
+	}
+
+}
