8c030e975eaf6cff6c13e085856b9ed8a2b724bd
rbarisozkal <barisozkal11@gmail.com>
2025-07-03 02:28:38 +0200

    feat(frontend-init): initialize frontend with Next.js, Tailwind CSS, and essential configurations including ESLint, TypeScript, and global styles

---
+++ humanoo-frontend/src/lib/providers.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import { useState } from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { Toaster } from "@/components/ui/sonner";
+
+export const Providers = ({ children }: { children: React.ReactNode }) => {
+  const [queryClient] = useState(() => new QueryClient());
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <Toaster richColors position="bottom-right" />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
+};
+export default Providers;
