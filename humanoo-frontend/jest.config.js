d59846989acbbb0ca267f85a28a48dc507d70ccc
rbarisozkal <barisozkal11@gmail.com>
2025-07-03 02:44:14 +0200

    fix(tests): update Jest configuration to use CommonJS module syntax

--- humanoo-frontend/jest.config.js
+++ humanoo-frontend/jest.config.js
@@ -1,4 +1,4 @@
-import nextJest from "next/jest";
+const nextJest = require("next/jest");
 
 const createJestConfig = nextJest({
   dir: "./",
@@ -13,4 +13,4 @@ const customJestConfig = {
   },
 };
 
-export default createJestConfig(customJestConfig);
+module.exports = createJestConfig(customJestConfig);
